982e258728b5a3585e04eae9d63f10b9d0a7b0c6
Andre Carvalho <90432705+AndreKvlh@users.noreply.github.com>
2026-04-07 15:16:42 -0300

    Criação da GUI

--- src/main/calculadora/Calculadora.java
+++ src/main/calculadora/Calculadora.java
@@ -3,12 +3,12 @@ package main.calculadora;
 import java.util.ArrayList;
 
 public class Calculadora {
-	private static double resultado = 0;
-	private static String operacaoAtual = "";
-	private static String numeroDigitado = "";
-	private static ArrayList<String> numeros = new ArrayList<>();
+	private double resultado = 0;
+	private String operacaoAtual = "";
+	private String numeroDigitado = "";
+	private ArrayList<String> numeros = new ArrayList<>();
 	
-	public static <T extends Number> double operacoesBasicas(T a, T b, String operador) {
+	public <T extends Number> double operacoesBasicas(T a, T b, String operador) {
 		double resultado;
 		switch (operador) {
 			case "+" -> resultado = a.doubleValue() + b.doubleValue();
@@ -23,23 +23,24 @@ public class Calculadora {
 		return resultado;
 	}
 	
-	public static void guardarNumero(String numero) {
+	public void guardarNumero(String numero) {
 		numeroDigitado = numero;
 		numeros.add(numeroDigitado);
 	}
 	
-	public static void guardarOperacao(String operacao) {
+	public void guardarOperacao(String operacao) {
 		operacaoAtual = operacao;
+		System.out.println(operacaoAtual);
 	}
 	
-	public static void processarOperacao() {
+	public void processarOperacao() {
 		double n1 = Double.parseDouble(numeros.get(0));
 		double n2 = Double.parseDouble(numeros.get(1));
 		resultado = operacoesBasicas(n1, n2, operacaoAtual);
 		System.out.printf("O resultado é %f", resultado);
 	}
 	
-	public static void converterEmPorcento() {
+	public void converterEmPorcento() {
 		double stringEmDouble = Double.parseDouble(numeroDigitado);
 		double numeroAnterior = Double.parseDouble(numeros.get(0));
 		double porcentagem = stringEmDouble / 100;
@@ -47,18 +48,28 @@ public class Calculadora {
 		numeros.set(numeros.size() - 1, numeroDigitado);
 	}
 	
-	public static void inverterSinal() {
+	public void inverterSinal() {
 		double stringEmDouble = Double.parseDouble(numeroDigitado);
 		double numeroInvertido = stringEmDouble * -1;
 		numeroDigitado = Double.toString(numeroInvertido);
 		numeros.set(numeros.size() - 1, numeroDigitado);
 	}
 	
-	public static void digitarNaTela(String valor) {
-		numeroDigitado += valor;
+	public void digitarNaTela(String valor) {
+		if(!numeroDigitado.contains(".") || this.eNumero(valor))numeroDigitado += valor;
+		System.out.println(numeroDigitado);
 	}
 	
-	public static void limpar(boolean tudo) {
+	public boolean eNumero(String valor) {
+		try {
+			Double.parseDouble(valor);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+	
+	public void limpar(boolean tudo) {
 		if(tudo) {
 			resultado = 0;
 			operacaoAtual = "";
@@ -67,7 +78,7 @@ public class Calculadora {
 		numeroDigitado = "";
 	}
 	
-	public static void deletarNumero() {
+	public void deletarNumero() {
 		if (numeroDigitado.length() > 0) {
 			numeroDigitado = numeroDigitado.substring(0, numeroDigitado.length() - 1);
 		}
--- src/main/gui/Main.java
+++ src/main/gui/Main.java
@@ -2,6 +2,13 @@ package main.gui;
 import java.util.Scanner;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import main.calculadora.Calculadora;
 
@@ -12,19 +19,36 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage palco) {
-		palco.setTitle("Olá, mundo!");
+		VBox base = new VBox(10);
+		base.setPadding(new Insets(10));
+		TextField visor = new TextField("0");
+		visor.setMaxWidth(Double.MAX_VALUE);
+		Scene cena = new Scene(base, 250, 400);
+		visor.setEditable(false);
+		visor.setAlignment(Pos.CENTER_RIGHT);
+		visor.setPrefHeight(70);
+		visor.setStyle("-fx-font-size: 28px;");
+		
+		GridPane teclado = new GridPane();
+		teclado.add(visor, 0, 0, 4, 1);
+		configurarTeclado(teclado);
+		
+		base.getChildren().addAll(visor, teclado);
+		
+		palco.setTitle("Calculadora");
+		palco.setScene(cena);
 		palco.show();
 	}
 
 	public static void main(String[] args) {
-		System.out.print("Digite um número: ");
+		/*System.out.print("Digite um número: ");
 		Calculadora.guardarNumero(checarEntrada(leitor));
 		System.out.print("Selecione a operação: +, -, *, / ");
 		String operacao = leitor.next();
 		Calculadora.guardarOperacao(operacao);
 		System.out.print("Digite um número: ");
 		Calculadora.guardarNumero(checarEntrada(leitor));
-		Calculadora.processarOperacao();
+		Calculadora.processarOperacao();*/
 		launch(args);
 	}
 
@@ -36,6 +60,42 @@ public class Main extends Application {
 				return entrada;
 			} catch (NumberFormatException e) {
 				System.out.println("ERRO! Digite um número válido");
+			}
+		}
+	}
+	
+	public void configurarTeclado(GridPane teclado) {
+		String[][] teclas = {
+				{"%","CE","C","Backspace"},
+				{"1/x", "x²", "²Vx", "/"},
+				{"7","8","9","*"},
+				{"4","5","6","-"},
+				{"1","2","3","+"},
+				{"+/-","0",".","="},
+		};
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 4; j++) {
+				Button botao = new Button(teclas[i][j]);
+				botao.setPrefSize(70,50);
+				
+				botao.setOnAction(e -> {
+					String tecla = botao.getText();
+					switch (tecla) {
+						case "1","2","3","4","5","6","7","8","9","0","." -> calculadora.digitarNaTela(tecla);
+						case "+","-","*","/" -> calculadora.guardarOperacao(tecla);
+						case "+/-" -> calculadora.inverterSinal();
+						case "%" -> calculadora.converterEmPorcento();
+						case "CE" -> calculadora.limpar(false);
+						case "C" -> calculadora.limpar(true);
+						case "Backspace" -> calculadora.deletarNumero();
+						case "1/x" -> calculadora.operacoesBasicas(Double.parseDouble(calculadora.getNumeroDigitado()), 0, "n");
+						case "x²" -> calculadora.operacoesBasicas(Double.parseDouble(calculadora.getNumeroDigitado()), 0, "^");
+						case "²Vx" -> calculadora.operacoesBasicas(Double.parseDouble(calculadora.getNumeroDigitado()), 0, "V");
+						default -> calculadora.processarOperacao();
+					}
+				});
+				
+				teclado.add(botao, j, i + 1);
 			}
 		}
 	}
